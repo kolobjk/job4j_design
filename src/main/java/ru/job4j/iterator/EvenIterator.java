5dc0448be1f2eeee93a3faa25ec77d8ef936470b
Aleksandr.Khvostov <aleksanfr.khvostov@glowbyteconsulting.com>
2021-02-23 13:50:44 +0300

    5.1.2. Создать итератор четные числа [#474211]

--- src/main/java/ru/job4j/iterator/EvenIterator.java
+++ src/main/java/ru/job4j/iterator/EvenIterator.java
@@ -10,16 +10,12 @@ public class EvenIterator implements Iterator<Integer> {
     public EvenIterator(int[] data) {
         this.data = data;
     }
-    public Integer newPointPosition() {
-        Integer returnPosition = point;
-        while (returnPosition < data.length && data[returnPosition] % 2 != 0) {
-            returnPosition++;
-        }
-        return  returnPosition;
-    }
     @Override
     public boolean hasNext() {
-        return  newPointPosition() < data.length;
+        while (point < data.length && data[point] % 2 != 0) {
+            point++;
+        }
+        return  point < data.length;
     }
 
     @Override
@@ -27,7 +23,6 @@ public class EvenIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        point = newPointPosition();
         return data[point++];
     }
 }
