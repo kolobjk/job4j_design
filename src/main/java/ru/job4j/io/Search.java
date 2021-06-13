e6f9b358a674bd835811d0977988127908ae7c66
Aleksandr.Khvostov <aleksanfr.khvostov@glowbyteconsulting.com>
2021-06-13 21:33:57 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,14 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith("xml")).forEach(System.out::println);
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder and file extension is null. Usage java -jar dir.jar ROOT_FOLDER FILE_EXTENSION.");
+        }
+        if (args.length == 1) {
+            throw new IllegalArgumentException("File extension is null. Usage java -jar dir.jar ROOT_FOLDER FILE_EXTENSION.");
+        }
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
